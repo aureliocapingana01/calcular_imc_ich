48f17cbfc33a434d224a0a0158c2107314ac8921
Aurelio Capingana <aurelio.capingana01@gmail.com>
2024-02-16 22:14:18 -0300

    feito o sistema responsivo do site

--- assets/js.script.js
+++ assets/js.script.js
@@ -2,46 +2,46 @@ let nome
 let altura
 let peso
 let imc
+let resultadoImc
+
+
 
 const calcularImc =  () => {
     nome = document.querySelector('#nome').value
     altura = document.querySelector('#altura').value
     peso = document.querySelector('#peso').value
     
+    resultadoImc = document.querySelector('.res')
     
     if (nome == '' || altura == '' || peso == '') {
-        console.log('Preencha os campos acima, por favor')
+        resultadoImc.innerHTML = 'Preencha os campos acima, por favor'
     }
     else if (altura == 0 || peso == 0) {
-        console.log('preencha os campos com informaçoes certas')
+        resultadoImc.innerHTML = 'preencha os campos com informaçoes certas'
     }
     else {
         imc = (peso / (altura * altura)).toFixed(2)
 
         if (imc < 18.5) {
-            console.log(`O seu IMC é ${imc}, Esta a baixo do normal, procure um medico`)
-            limpaCampo()
+            resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Esta a baixo do normal <br> <br> <br> procure um medico`
+            // limpaCampo()
         }
         else if (imc == 18.5 || 24.9 >= imc) {
-            console.log(`O seu IMC é ${imc}, Parabés seu peso esta normal`)
-            limpaCampo()
+            resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Parabéns <br> <br> <br> O seu IMC está Normal`
+    
         }
         else if (imc < 25.0 || 29.9 >= imc) {
-            console.log(`O seu IMC é ${imc}, Esta no Sobrepeso`)
-            limpaCampo()
+            resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Esta no Sobrepeso <br> <br> <br> Cuide da sua saúde, faça academia`
         }
         else if (imc < 30.0 || 34.9 >= imc) {
-            console.log(`O seu IMC é ${imc}, Esta no Obesidade grau I, cuide mais da sua saude`)
-            limpaCampo()
+            resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Esta na Obesidade de grau I <br> <br> <br> procure um medico, cuida-se muito`
         }
         else if (imc < 35.0 || 39.9 >= imc) {
-            console.log(`O seu IMC é ${imc}, Esta no Obesidade grau II, sua saúde pode esta em perigo`)
+            resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Esta na Obesidade  de grau II <br> <br> <br> procure um medico urgentimente`
         }
         else{
-            // console.log(`O teu imc é ${imc}`)
-            if (imc > 40.0) {
-                console.log(`O seu IMC é ${imc}, Esta no SObesidade grau III, sua saude está em um estado delicado`)
-            }
+                resultadoImc.innerHTML = `Boas Vindas <strong>${nome}</strong> <br> O seu IMC é <strong>${imc}</strong>, Esta na SObesidade do grau III <br> <br> <br> sua saude está em um estado delicado`
+ 
 
         }
     
